b5dab7a8e554b3b8e836404da8d892501cf5b0d1
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-14 23:04:08 +0300

    integers

--- index.dart
+++ index.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 void main() {
-    stdout.writeln("Hi, my name is: ?");
-    String? name = stdin.readLineSync();
-    print("My name is $name");
+    // stdout.writeln("Hi, my name is: ?");
+    // String? name = stdin.readLineSync();
+    // print("My name is $name");
 
   //variables
   String firstName = "Bloom";
@@ -10,5 +10,11 @@ void main() {
 
   print("My name is $firstName $lastName");
 
-  
+  //data types
+  var number = 2;
+  double number2 = 10.22;
+  int number3 = 22;
+  var total = number + number2 +number3;
+
+  print("The total of ($number+$number2+$number3) is $total");
 }
