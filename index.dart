c057f597b8f86526a81782d64878081cbf21cb7f
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-12-14 23:00:41 +0300

    variables

--- index.dart
+++ index.dart
@@ -3,4 +3,12 @@ void main() {
     stdout.writeln("Hi, my name is: ?");
     String? name = stdin.readLineSync();
     print("My name is $name");
+
+  //variables
+  String firstName = "Bloom";
+  var lastName = 'Tauta';
+
+  print("My name is $firstName $lastName");
+
+  
 }
